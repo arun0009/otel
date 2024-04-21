7f6de210a80bf7565d5d73e986b814865a02f0ec
Arun Gopalpuri <arun@gopalpuri.com>
2024-04-21 02:11:45 -0700

    added http to the mix

--- app.ts
+++ app.ts
@@ -6,16 +6,16 @@ import {
   ReceiveMessageCommand,
   DeleteMessageCommand,
 } from "@aws-sdk/client-sqs";
+import axios from "axios";
 
-// -------------------firing express app
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("/send", async (request: Request, response: Response) => {
-  console.log("inside send"); 
-  const sqs = new SQSClient({});
+  console.log("inside send");
+  const sqs = new SQSClient({ endpoint: "http://localstack:4566" });
 
   const publishCommand = new SendMessageCommand({
     QueueUrl: "http://localstack:4566/000000000000/otel-queue",
@@ -23,30 +23,40 @@ app.get("/send", async (request: Request, response: Response) => {
   });
 
   const sqsResponse = await sqs.send(publishCommand);
-  console.log(JSON.stringify(sqsResponse));
-  response.json(JSON.stringify(sqsResponse));
+  response.json({ messageId: sqsResponse.MessageId });
 });
 
 app.get("/receive", async (request: Request, response: Response) => {
-  const sqs = new SQSClient({});
+  const sqs = new SQSClient({ endpoint: "http://localstack:4566" });
 
   const receiveCommand = new ReceiveMessageCommand({
     QueueUrl: "http://localstack:4566/000000000000/otel-queue",
-    MessageAttributeNames: ['All'],
+    MessageAttributeNames: ["All"],
     MaxNumberOfMessages: 1,
     VisibilityTimeout: 30,
   });
 
   const sqsResponse = await sqs.send(receiveCommand);
-  sqsResponse.Messages?.forEach((message) => async () => {
-        console.log(JSON.stringify(message.Body));
-        const deleteCommand = new DeleteMessageCommand({
-            QueueUrl: "http://localstack:4566/000000000000/otel-queue",
-            ReceiptHandle: message.ReceiptHandle,
-        });
-        await sqs.send(deleteCommand);
+  sqsResponse.Messages?.forEach((message) => {
+    console.log(message.Body);
+    const deleteCommand = new DeleteMessageCommand({
+      QueueUrl: "http://localstack:4566/000000000000/otel-queue",
+      ReceiptHandle: message.ReceiptHandle,
     });
-    response.json({message: `done`});
+
+    const options = {
+      host: "echo",
+      port: 5678,
+    };
+
+    axios.get("http://echo:5678").then((res) => {
+      console.log("Received response from server", res.status);
+    });
+
+    sqs.send(deleteCommand);
+    console.log("delete done!");
+    response.json({ message: `done` });
+  });
 });
 
 app.get("/home", (request: Request, response: Response) => {
--- otel.ts
+++ otel.ts
@@ -1,12 +1,16 @@
-import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
 import { AwsInstrumentation } from "@opentelemetry/instrumentation-aws-sdk";
-import { Span } from "@opentelemetry/api";
-import { parseTraceParent } from "@opentelemetry/core";
-import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
-import { ExpressLayerType, ExpressInstrumentation } from "@opentelemetry/instrumentation-express";
+import {
+  ExpressLayerType,
+  ExpressInstrumentation,
+} from "@opentelemetry/instrumentation-express";
+import { Resource } from "@opentelemetry/resources";
+const {
+  SemanticResourceAttributes,
+} = require("@opentelemetry/semantic-conventions");
 
 const exporter = new OTLPTraceExporter({
   url: "http://otel-collector:4318/v1/traces",
@@ -15,22 +19,32 @@ const exporter = new OTLPTraceExporter({
 
 console.log("starting otel layer");
 
+const resource = Resource.default().merge(
+  new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: "otel",
+    [SemanticResourceAttributes.SERVICE_VERSION]: "0.1.0",
+  })
+);
+
 const sdk = new NodeSDK({
+  resource: resource,
   spanProcessor: new BatchSpanProcessor(exporter),
   serviceName: "otel",
   instrumentations: [
     new HttpInstrumentation(),
-    new ExpressInstrumentation({ ignoreLayersType: [ExpressLayerType.MIDDLEWARE] }),
+    new ExpressInstrumentation({
+      ignoreLayersType: [ExpressLayerType.MIDDLEWARE],
+    }),
     new AwsInstrumentation({
       sqsExtractContextPropagationFromPayload: true,
       suppressInternalInstrumentation: true,
-      sqsProcessHook: (span: Span, { message }) => {
-        const traceparent = message.MessageAttributes?.traceparent.StringValue;
-        if (traceparent) {
-          const spanContext = parseTraceParent(traceparent);
-          span.setAttribute("parentTraceId", spanContext?.traceId || "");
-        }
-      },
+      // sqsProcessHook: (span: Span, { message }) => {
+      //   const traceparent = message.MessageAttributes?.traceparent.StringValue;
+      //   if (traceparent) {
+      //     const spanContext = parseTraceParent(traceparent);
+      //     span.setAttribute("parentTraceId", spanContext?.traceId || "");
+      //   }
+      // },
     }),
   ],
 });
